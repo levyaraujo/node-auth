3849e70af55d8bd56b750f8980e9d9597c49f2ca
Levy <lev0x79@gmail.com>
2025-01-20 10:42:33 -0300

    implementa controller de signup

---
+++ src/application/controllers/SignUpController.ts
@@ -0,0 +1,37 @@
+import { z } from "zod"
+
+import { IController, IRequest, IResponse } from "../interfaces/IController";
+import { SignUpUseCase } from "../useCases/SignUpUseCase";
+
+
+const schema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export class SignUpController implements IController {
+  async handle({ body }: IRequest): Promise<IResponse> {
+    try {
+      const { name, email, password } = schema.parse(body);
+
+      const signUpUseCase = new SignUpUseCase();
+      signUpUseCase.execute({ name, email, password });
+
+      return {
+        statusCode: 201,
+        body: { "message": "User created successfully" },
+      };
+    }
+    catch (error) {
+      if (error instanceof z.ZodError) {
+        return {
+          statusCode: 400,
+          body: error.issues,
+        };
+      }
+      throw error;
+    }
+
+  }
+}
