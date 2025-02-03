56109f0ffd9ea27247a61b489453d969fdec3e1c
Levy <lev0x79@gmail.com>
2025-02-03 00:56:14 -0300

    refatora server para usar routeAdapter e factories

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,32 +1,25 @@
 import express from 'express';
-import { SignUpController } from '../application/controllers/SignUpController';
-import { SignInController } from '../application/controllers/SignInController';
-import { SignInUseCase } from '../application/useCases/SignInUseCase';
-import { SignUpUseCase } from '../application/useCases/SignUpUseCase';
+import { middlewareAdapter } from './adapters/middlewareAdapter';
+import { makeAuthenticationMiddleware } from '../factories/makeAuthenticationMiddleware';
+import { makeSignUpController } from '../factories/makeSignUpController';
+import { routeAdapter } from '../application/lib/routeAdapter';
+import { makeUserProfileController } from '../factories/makeUserProfileController';
+import { makeSignInController } from '../factories/makeSignInController';
 
 
 const app = express();
 app.use(express.json());
 
 
-app.post('/sign-up', async (request, response) => {
-  const signUpController = new SignUpController(new SignUpUseCase());
+app.post('/sign-up', routeAdapter(makeSignUpController()));
+
+app.post('/sign-in', routeAdapter(makeSignInController()))
 
 
-  const { statusCode, body } = await signUpController.handle({ body: request.body })
-
-  response.status(statusCode).json(body);
-})
-
-app.post('/sign-in', async (request, response) => {
-  const signInController = new SignInController(new SignInUseCase());
-
-
-  const { statusCode, body } = await signInController.handle({ body: request.body })
-
-  response.status(statusCode).json(body);
-})
-
+app.get('/profile',
+  middlewareAdapter(makeAuthenticationMiddleware()),
+  routeAdapter(makeUserProfileController()),
+);
 
 
 app.listen(3000, () => {
